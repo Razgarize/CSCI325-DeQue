92e0d17d65bc577142714ebabeb6182dc4708bc1
razgarize <jhart44@ut.utm.edu>
2024-04-26 07:18:27 -0500

    Fully functional Deque system! Just need clean up, a proper print function and documentation!

--- Bonus/Template_deque.h
+++ Bonus/Template_deque.h
@@ -9,13 +9,13 @@ private:
     DequeT **blockmap;   // Pointer to the map of pointers to the blocks
     DequeT *RearPtr;     // points to the last element in the deque
     DequeT *FrontPtr;    // points to the first element in the deque
-    DequeT *MapFrontPtr; // Front most pointer in the map
-    DequeT *MapRearPtr;  // Rear most pointer in the map
     int capacity;
     int BlockElementCounter; // Counter for the number of elements in all the blocks.
     int MapSize;
     const static int BLOCK_SIZE = 5; // Size of each block in the arrays. This is a constant value that will never change for simplicity.
     void MapResize();
+    void MapResizeRight();
+    void MapResizeLeft();
 
 public:
     Deque();
@@ -23,23 +23,28 @@ public:
     void push_element_front(DequeT value);
     void push_element_back(DequeT value);
     void pop_front();
-    void push_back(DequeT value);
     void pop_back();
     void get_front();
     void get_back();
-    void empty();
-    DequeT get_capacity();
-    DequeT get_MapSize();
+    bool empty();
+    int get_capacity();
+    int get_MapSize();
     void MapResizeHelper();
     DequeT &operator[](DequeT index);
     void MoveRearPtrRight();
     void MoveFrontPtrLeft();
     void NewBlockFront();
     void NewBlockRear();
+    void print();
 
     // DEBUGGER SECTION:
     void printBlockMap();
 };
+
+
+
+
+
 
 // Implimentation of the templated Deque class
 
@@ -52,8 +57,7 @@ Deque<DequeT>::Deque() // Constructor
     BlockElementCounter = 0;           // Number of elements in the blocks
     RearPtr = &blockmap[0][0];         // Points to the last element in the deque. 
     FrontPtr = &blockmap[0][0];        // Points to the first element in the deque. Starts at the same place as RearPtr.
-    MapFrontPtr = NULL;                // Front most pointer in the map
-    MapRearPtr = NULL;                 // Rear most pointer in the map
+    
 }
 
 
@@ -61,6 +65,94 @@ template <typename DequeT>
 Deque<DequeT>::~Deque() // Destructor
 {
     delete[] blockmap;
+}
+
+
+template <typename DequeT>
+void Deque<DequeT>::get_back()
+{
+    std::cout << "Back: " << *RearPtr << std::endl;
+}
+
+template <typename DequeT>
+void Deque<DequeT>::pop_back()
+{
+  if(BlockElementCounter == 0)
+    {
+      std::cout << "POP BACK ERROR: There are no elements in the DeQue!" << std::endl;
+      return;
+    }
+    else
+    {
+    for(int i = 0; i < BLOCK_SIZE; i++)
+    {
+      std::cout << "for loop: Pop Back: " << i << std::endl;
+      if(RearPtr == &blockmap[MapSize - 1][0]) // If the RearPtr is at the end of the block (Our ultimate end goal).
+      {
+        //Once we get to the end of the block, we will delete the block and move the RearPtr to the last element of the now last block.
+        delete[] blockmap[MapSize - 1]; // Delete the block
+        MapSize--; // Decrement the MapSize
+        RearPtr = &blockmap[MapSize - 1][BLOCK_SIZE - 1]; // Set the RearPtr to the last element in the new block
+        BlockElementCounter--; // Decrement the number of elements in the blocks
+        return; // Exit the function and end the loop
+      }
+      else
+      {
+        RearPtr--; // Move the RearPtr to the left
+        BlockElementCounter--; // Decrement the number of elements in the blocks
+      }
+    }
+}
+    
+}
+
+template <typename DequeT>
+void Deque<DequeT>::get_front()
+{
+    if(BlockElementCounter == 0)
+    {
+      std::cout << "GET FRONT ERROR: There are no elements in the DeQue!" << std::endl;
+      return;
+    }
+    std::cout << "Front: " << *FrontPtr << std::endl;
+}
+
+
+template <typename DequeT>
+void Deque<DequeT>::pop_front()
+{
+    for(int i = 0; i < BLOCK_SIZE; i++)
+    {
+      std::cout << "for loop: Pop Front: " << i << std::endl;
+      if(FrontPtr == &blockmap[0][BLOCK_SIZE - 1]) // If the FrontPtr is at the beginning of the block (Our ultimate end goal).
+      {
+        //Once we get to the beginning of the block, we will delete the block and move the FrontPtr to the first element of the now first block.
+        delete[] blockmap[0]; // Delete the block
+        MapSize--; // Decrement the MapSize
+        FrontPtr = &blockmap[0][0]; // Set the FrontPtr to the first element in the new block
+        BlockElementCounter--; // Decrement the number of elements in the blocks
+        return; // Exit the function and end the loop
+      }
+      else
+      {
+        FrontPtr++; // Move the FrontPtr to the right
+        BlockElementCounter--; // Decrement the number of elements in the blocks
+      }
+    }
+}
+
+
+template <typename DequeT>
+bool Deque<DequeT>::empty()
+{
+  if(BlockElementCounter <= 0)
+    {
+      return true;
+    }
+  else
+    {
+      return false;
+    }
 }
 
 template <typename DequeT>
@@ -75,21 +167,52 @@ void Deque<DequeT>::NewBlockRear()
         MapResize(); // Resize the map to double the capacity
     }
     blockmap[MapSize] = new DequeT[BLOCK_SIZE]; // Create a new block of size BLOCK_SIZE
-    MapSize++;                                  // Increment the MapSize of the map
+    for(int i = 0; i < BLOCK_SIZE; i++)
+    {
+        blockmap[MapSize][i] = 0;
+    }
+    MapSize++; // Increment the MapSize of the map
+}
+
+template <typename DequeT>
+void Deque<DequeT>::NewBlockFront()
+{
+  if (blockmap == nullptr) // If the map is empty
+    {
+      blockmap = new DequeT *[capacity]; // Create a new map of pointers to the blocks
+    }
+  if (MapSize >= capacity) // If the map is full
+    {
+      MapResize(); // Resize the map to double the capacity
+
+    }
+  else
+    {
+      MapResize();
+      blockmap[0] = new DequeT[BLOCK_SIZE]; //adds a new block to the start of the map.
+      for(int i = 0; i < BLOCK_SIZE; i++)
+        {
+            blockmap[0][i] = 0;
+        }
+        MapSize++; // Increment the MapSize of the map
+    }
+  
 }
 
 template <typename DequeT>
 void Deque<DequeT>::push_element_front(DequeT value)
 {
-    if (FrontPtr == blockmap[0]) // If the FrontPtr is at the front of the block
+    if (FrontPtr == blockmap[0]) // If the FrontPtr is at the beginning of the block
     {
+      //std::cout << "PUSH: FrontPtr is at the beginning of the block" << std::endl;
         NewBlockFront(); // Create a new block at the front
-        FrontPtr = &blockmap[MapSize - 1][BLOCK_SIZE - 1]; // Set the FrontPtr to the last element in the new block
+        FrontPtr = &blockmap[0][BLOCK_SIZE - 1]; // Set the FrontPtr to the last element in the new block
         *FrontPtr = value; // Set the value of the FrontPtr to the value passed in
         BlockElementCounter++; // Increment the number of elements in the blocks
     }
     else
     {
+      //std::cout << "PUSH: FrontPtr is moving left" << std::endl;
         FrontPtr--; // Move the FrontPtr to the left
         *FrontPtr = value; // Set the value of the FrontPtr to the value passed in
         BlockElementCounter++; // Increment the number of elements in the blocks
@@ -99,7 +222,7 @@ void Deque<DequeT>::push_element_front(DequeT value)
 template <typename DequeT>
 void Deque<DequeT>::push_element_back(DequeT value)
 {
-    if (RearPtr == nullptr || RearPtr == &blockmap[MapSize - 1][BLOCK_SIZE - 1]) // If the RearPtr is at the back of the block
+    if (RearPtr == nullptr or RearPtr == &blockmap[MapSize - 1][BLOCK_SIZE]) // If the RearPtr is at the back of the block
     {
         if (MapSize >= capacity) // If the map is full
         {
@@ -113,25 +236,15 @@ void Deque<DequeT>::push_element_back(DequeT value)
     RearPtr++; // Move the RearPtr to the right
 }
 
-
-
 template <typename DequeT>
-void Deque<DequeT>::NewBlockFront()
+int Deque<DequeT>::get_capacity()
 {
-  if (blockmap == nullptr) // If the map is empty
-    {
-      blockmap = new DequeT *[capacity]; // Create a new map of pointers to the blocks
-    }
-  if (MapSize >= capacity) // If the map is full
-    {
-      MapResize(); // Resize the map to double the capacity
-    }
-  else
-    {
-      MapResize();
-      blockmap[0] = new DequeT[BLOCK_SIZE]; //adds a new block to the start of the map.
-    }
-  MapSize++; // Increment the MapSize of the map
+    return capacity;
+}
+template <typename DequeT>
+int Deque<DequeT>::get_MapSize()
+{
+    return MapSize;
 }
 
 template <typename DequeT>
@@ -139,17 +252,19 @@ void Deque<DequeT>::MoveFrontPtrLeft()
 {
     if (blockmap == nullptr) // If the map is empty
     {
-        std::cout << "Blockmap is empty" << std::endl;
+      //std::cout << "Blockmap is empty" << std::endl;
         return;
     }
     if (FrontPtr == blockmap[0][0])
     {
+      //std::cout << "FrontPtr is at the beginning of the block" << std::endl;
         NewBlockFront();
         FrontPtr--;
         return;
     }
     else
     {
+      //std::cout << "FrontPtr is moving left" << std::endl;
         FrontPtr--;
         return;
     }
@@ -158,34 +273,53 @@ void Deque<DequeT>::MoveFrontPtrLeft()
 template <typename DequeT>
 void Deque<DequeT>::MapResize()
 {
-    DequeT **temp = new DequeT *[capacity * 2]; // Create a new map of pointers to the blocks
+    DequeT **temp = new DequeT *[capacity + 2]; // Create a new map of pointers to the blocks with new capacity
     for (int i = 0; i < MapSize; i++)
     {
         temp[i + 1] = blockmap[i]; // Copy the pointers from the old map to the new map
     }
     delete[] blockmap; // Delete the old map
     blockmap = temp;   // Set the old map to the new map
-    capacity *= 2;     // Double the capacity
+    capacity += 2;     // Increases the capacity by 2
 }
 
-template <typename DequeT>
-void Deque<DequeT>::get_front()
-{
-    std::cout << *FrontPtr << std::endl;
-}
 
+
+//TODO: Implement the get_back() function
 template <typename DequeT>
 DequeT &Deque<DequeT>::operator[](DequeT index) // Overloaded operator[] to access elements in the deque
 {
-    return blockmap[index];
+    if (index < 0 or index >= BlockElementCounter) // If the index is out of bounds
+    {
+        std::cout << "ACCESS ERROR: Index out of bounds" << std::endl; // DEBUGGER: Remove when finished
+        return blockmap[0][0]; // Return the first element in the first block
+    }
+    int block = index / BLOCK_SIZE; // Calculate the block number'
+    int blockIndex = index % BLOCK_SIZE; // Calculate the index in the block
+    return blockmap[block][blockIndex]; // Return the element at the index
 }
 
 template <typename DequeT>
 void Deque<DequeT>::printBlockMap()
 {
+  if(blockmap == nullptr)
+    {
+      std::cout << "PRINT ERROR: Blockmap is empty" << std::endl;
+      return;
+    }
+    if(MapSize == 0)
+    {
+      std::cout << "PRINT ERROR: Blockmap is empty" << std::endl;
+      return;
+    }
+    if(BlockElementCounter == 0)
+    {
+      std::cout << "PRINT ERROR: There are no inserted elements in the DeQue!" << std::endl;
+      return;
+    }
     for (int i = 0; i < MapSize; i++)
     {
-        std::cout << "Blockmap[" << i << "] = " << blockmap[i] << " | and the values are: ";
+        std::cout << "Blockmap[" << i << "] = " << &blockmap[i] << " | and the values are: ";
         for (int j = 0; j < BLOCK_SIZE; j++)
         {
             std::cout << blockmap[i][j] << " ";
--- Bonus/main_bonus.cpp
+++ Bonus/main_bonus.cpp
@@ -4,15 +4,74 @@
 
 using namespace std;
 
+template <typename DequeT>
+void Test(DequeT &d);
+
+template <typename DequeT>
+void Test2(DequeT &d);
+
+template <typename DequeT>
+void TestString(DequeT &d);
+
 int main() {
     Deque<int> d;
-    std::cout << "Test" << std::endl;
+    d.push_element_front(90);
+    d.printBlockMap();
+    d.get_front();
+    d.pop_front();
+    d.printBlockMap();
+    d.get_front();
+    d.push_element_back(91);
+    d.push_element_front(90);
+    d.printBlockMap();
+    //Test2(d);
+    //Deque<char> t;
+    //Test2(t);
+    //TestString(t);
+    // cout << "Front: ";
+    // d.get_front();
+    cout << "Hello, World!" << endl;
+    return 0;
+}
+
+
+
+
+template <typename DequeT>
+void Test2(DequeT &d)
+{
+    for(int i = 33; i < 126; i++)
+    {
+        // cout << "Capacity: " << d.get_capacity() << endl;
+        // cout << "Element: " << i << " " << endl;
+        d.push_element_front(i);
+    }
+    //d.printBlockMap();
+    // for(int i = 0; i < 1; i++)
+    // {
+    //     // cout << "Capacity: " << d.get_capacity() << endl;
+    //     // cout << "Element: " << i << " " << endl;
+    //     d.push_element_back(i);
+    // }
+    d.printBlockMap();
+    cout << endl;
+    cout << "Random Access: " << d[2] << endl;
+}
+
+
+
+template <typename DequeT>
+void Test(DequeT &d)
+{
+        std::cout << "Test" << std::endl;
     //  for(int i = 0; i < 199; i++)
     // {
     //     cout << "Capacity: " << d.get_capacity() << endl;
     //     cout << i << " " << endl;
     //     d.push_element_front(i);
     // }
+    cout << "Map Size: " << d.get_MapSize() << endl;
+    cout << "Map Capacity: " << d.get_capacity() << endl;
      d.push_element_front(200);
      d.printBlockMap();
      cout << "Test" << endl;
@@ -24,10 +83,11 @@ int main() {
      d.push_element_front(196);
      d.printBlockMap();
      d.push_element_front(195);
+     d.push_element_back(203);
+    d.push_element_back(204);
+    d.push_element_back(205);
+    d.push_element_back(206);
+    d.push_element_front(240);
      cout << "Test" << endl;
      d.printBlockMap();
-    // cout << "Front: ";
-    // d.get_front();
-    cout << "Hello, World!" << endl;
-    return 0;
 }
