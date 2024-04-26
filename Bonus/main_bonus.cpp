05d6c4772fddf95efa1617b3f4242b05de5d309b
razgarize <jhart44@ut.utm.edu>
2024-04-26 15:14:31 -0500

    Added a print feature. Fixed some bugs. Documentation is now needed

--- Bonus/main_bonus.cpp
+++ Bonus/main_bonus.cpp
@@ -14,24 +14,15 @@ template <typename DequeT>
 void TestString(DequeT &d);
 
 int main() {
-    Deque<int> d;
-    d.push_element_front(90);
-    d.printBlockMap();
-    d.get_front();
-    d.pop_front();
-    d.printBlockMap();
-    d.get_front();
-    d.push_element_back(91);
-    d.push_element_front(90);
-    d.printBlockMap();
-    //Test2(d);
-    //Deque<char> t;
-    //Test2(t);
-    //TestString(t);
-    // cout << "Front: ";
-    // d.get_front();
-    cout << "Hello, World!" << endl;
-    return 0;
+  Deque<int> d;
+  Test2(d);
+  //Deque<char> t;
+  //Test2(t);
+  //TestString(t);
+  // cout << "Front: ";
+  // d.get_front();
+  cout << "Hello, World!" << endl;
+  return 0;
 }
 
 
@@ -40,22 +31,22 @@ int main() {
 template <typename DequeT>
 void Test2(DequeT &d)
 {
-    for(int i = 33; i < 126; i++)
+  for(int i = 33; i < 126; i++)
     {
-        // cout << "Capacity: " << d.get_capacity() << endl;
-        // cout << "Element: " << i << " " << endl;
-        d.push_element_front(i);
+      // cout << "Capacity: " << d.get_capacity() << endl;
+      // cout << "Element: " << i << " " << endl;
+      d.push_element_front(i);
     }
-    //d.printBlockMap();
-    // for(int i = 0; i < 1; i++)
-    // {
-    //     // cout << "Capacity: " << d.get_capacity() << endl;
-    //     // cout << "Element: " << i << " " << endl;
-    //     d.push_element_back(i);
-    // }
-    d.printBlockMap();
-    cout << endl;
-    cout << "Random Access: " << d[2] << endl;
+  //d.printBlockMap();
+  // for(int i = 0; i < 1; i++)
+  // {
+  //     // cout << "Capacity: " << d.get_capacity() << endl;
+  //     // cout << "Element: " << i << " " << endl;
+  //     d.push_element_back(i);
+  // }
+  d.print();
+  cout << endl;
+  cout << "Random Access: " << d[2] << endl;
 }
 
 
@@ -63,31 +54,31 @@ void Test2(DequeT &d)
 template <typename DequeT>
 void Test(DequeT &d)
 {
-        std::cout << "Test" << std::endl;
-    //  for(int i = 0; i < 199; i++)
-    // {
-    //     cout << "Capacity: " << d.get_capacity() << endl;
-    //     cout << i << " " << endl;
-    //     d.push_element_front(i);
-    // }
-    cout << "Map Size: " << d.get_MapSize() << endl;
-    cout << "Map Capacity: " << d.get_capacity() << endl;
-     d.push_element_front(200);
-     d.printBlockMap();
-     cout << "Test" << endl;
-     d.push_element_back(201);
-     d.push_element_back(202);
-     d.push_element_front(199);
-     d.push_element_front(198);
-     d.push_element_front(197);
-     d.push_element_front(196);
-     d.printBlockMap();
-     d.push_element_front(195);
-     d.push_element_back(203);
-    d.push_element_back(204);
-    d.push_element_back(205);
-    d.push_element_back(206);
-    d.push_element_front(240);
-     cout << "Test" << endl;
-     d.printBlockMap();
+  std::cout << "Test" << std::endl;
+  //  for(int i = 0; i < 199; i++)
+  // {
+  //     cout << "Capacity: " << d.get_capacity() << endl;
+  //     cout << i << " " << endl;
+  //     d.push_element_front(i);
+  // }
+  cout << "Map Size: " << d.get_MapSize() << endl;
+  cout << "Map Capacity: " << d.get_capacity() << endl;
+  d.push_element_front(200);
+  d.printBlockMap();
+  cout << "Test" << endl;
+  d.push_element_back(201);
+  d.push_element_back(202);
+  d.push_element_front(199);
+  d.push_element_front(198);
+  d.push_element_front(197);
+  d.push_element_front(196);
+  d.printBlockMap();
+  d.push_element_front(195);
+  d.push_element_back(203);
+  d.push_element_back(204);
+  d.push_element_back(205);
+  d.push_element_back(206);
+  d.push_element_front(240);
+  cout << "Test" << endl;
+  d.printBlockMap();
 }
